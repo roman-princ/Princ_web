2def0a00f223efca5ab0f5edf6ae49dbda6bb1b1
Roman Princ <u19.421@pslib.cloud>
2022-02-08 12:28:36 +0100

    sjhdfkjsdhfnk

--- web/js/cursor.js
+++ web/js/cursor.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Od roku 1992.","...pro vaši bezpečnost a pohodlí..."];
+const textArray = ["OD ROKU 1992","...pro vaši bezpečnost a pohodlí..."];
 const typingDelay = 100;
 const erasingDelay = 50;
 const newTextDelay = 2000; // Delay between current and next text
